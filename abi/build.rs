eee9aa7a5989e21c718d91128468f1a6e65e8eed
yjh <860622588@qq.com>
2024-01-10 09:52:48 +0800

    feat: add build.rs

--- abi/build.rs
+++ abi/build.rs
@@ -1,1 +1,15 @@
-fn main() {}
+use std::process::Command;
+
+fn main() {
+    tonic_build::configure()
+        .out_dir("src/pb")
+        .compile(&["./protos/reservation.proto"], &["protos"])
+        .unwrap();
+
+    Command::new("cargo")
+        .args(["fmt"])
+        .output()
+        .expect("Failed to run cargo fmt");
+
+    println!("cargo:rerun-if-changed=protos/reservation.proto");
+}
